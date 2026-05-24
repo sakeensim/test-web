b0e4e50abe8fc7a996c74ce26315ac8574ac607b
sakeensim <sakeensim@gmail.com>
2026-05-25 01:17:58 +0700

    fix marker from leaflet

--- src/adminPage/OrganizationSettings.jsx
+++ src/adminPage/OrganizationSettings.jsx
@@ -19,6 +19,16 @@ import API_URL from '../utils/api'
 import useAuthStore from '../store/auth-store'
 import { createAlert } from '../utils/createAlert'
 
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
 const DEFAULT_FORM = {
   name: '',
   code: '',
